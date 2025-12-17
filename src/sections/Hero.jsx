7e3fd9ba62d11de90dfa3e2518a195de76830787
rohan <rohan8112677@gmail.com>
2025-12-17 22:51:14 +0530

    Initial commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,79 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import Button from "../components/Button";
+import { words } from "../constants";
+
+
+const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+    );
+  });
+
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="" />
+      </div>
+
+      <div className="hero-layout">
+        {/* LEFT: Hero Content */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
+
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I’m Rohan Soni, a developer with a passion for
+              code.
+            </p>
+
+            <Button 
+              text="Experience"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="work"/>
+          </div>
+        </header>
+
+        {/* RIGHT: 3D Model or Visual */}
+        <figure>
+          <div className="hero-3d-layout">
+            <img src="https://i.pinimg.com/736x/1c/57/bf/1c57bf4773c1b66ac8608f456feceed2.jpg" style={{height:600,position:"absolute" ,top:"50%",left:"50%",transform:"translate(-50%, -50%)",zIndex:1  ,objectFit:"cover" ,width:"600px",borderRadius:"10px"}}/>
+          </div>
+        </figure>
+      </div>
+
+     <div id="counter">
+
+     </div>
+    </section>
+  );
+};
+
+export default Hero;
